8ea2a42595f22b486eaf25f33679470d1dc8cd4d
Alexej Grohe <alexej@Alexejs-MacBook-Pro.local>
2015-11-04 14:00:43 +0100

    init

---
+++ rules.js
@@ -0,0 +1,10 @@
+{
+  //percentage of points that will be redistributed
+  redistribution_rate : 0.1,
+  //time in minutes after redistribution happens
+  redistribution_interval : 60,
+  //amount of points that will be created
+  creation_rate : 10,
+  //time in minutes after new points will be created
+  creation_interval : 60
+}
---
+++ server.js
@@ -0,0 +1,24 @@
+// server.js
+
+// BASE SETUP
+// =============================================================================
+
+// call the packages we need
+var express    = require('express');        // call express
+var app        = express();                 // define our app using express
+var bodyParser = require('body-parser');
+var github = require('github');
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;        // set our port
+
+
+
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log('Magic happens on port ' + port);
