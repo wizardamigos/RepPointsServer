4f958f4d8cf012a056fc87da96a399dd4f23e1e1
Alexej Grohe <alexej@Alexejs-MacBook-Pro.local>
2015-11-05 19:09:02 +0100

    running

--- engine.js
+++ engine.js
@@ -5,6 +5,9 @@ function engine(){
 
 var _this = this;
 
+var dis_snap_path = "data/dis_snapshot.json";
+var rules_path = "data/rules.json";
+
 var dis_snapshot, rules;
 
 var redistribute_timer_id;
@@ -26,19 +29,51 @@ var creation_timer_id;
   }
 
   this.loadData = function(){
-      dis_snapshot = JSON.parse(fs.readFileSync('dis_snapshot.json', 'utf8'));
+      dis_snapshot = JSON.parse(fs.readFileSync(dis_snap_path, 'utf8'));
       console.log("read dis_snapshot");
 
-      rules = JSON.parse(fs.readFileSync('rules.json', 'utf8'));
+      rules = JSON.parse(fs.readFileSync(rules_path, 'utf8'));
       console.log("read rules");
   }
 
   this.redistribute = function(){
     console.log("redistribute");
+
+    var sum = 0;
+
+    dis_snapshot.users.forEach(function(e){
+      var r;
+      sum += r = e.points * rules.redistribution_rate;
+      e.points -= r;
+    });
+
+    dis_snapshot.users.forEach(function(e, i, arr){
+      e.points += sum / arr.length;
+    });
+
+    dis_snapshot.timestamp = Date.now();
+
+    fs.writeFile(dis_snap_path, JSON.stringify(dis_snapshot, null, 4), function(err){
+      if(err){
+        console.log(err);
+      }
+    });
   }
 
   this.createPoints = function(){
     console.log("create points");
+
+    dis_snapshot.users.forEach(function(e, i, arr){
+      e.points += rules.creation_rate / arr.length;
+    });
+
+    dis_snapshot.timestamp = Date.now();
+
+    fs.writeFile(dis_snap_path, JSON.stringify(dis_snapshot, null, 4), function(err){
+      if(err){
+        console.log(err);
+      }
+    });
   }
 
 }
