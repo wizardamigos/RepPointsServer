713093f03801997ae283b28375cccf02bd0ed763
alexej.grohe@gmail.com <alexej.grohe@gmail.com>
2015-11-10 19:58:14 +0000

    git init from heroku

---
+++ rest_api.js
@@ -0,0 +1,25 @@
+// server.js
+
+// BASE SETUP
+// =============================================================================
+
+// call the packages we need
+var express    = require('express');        // call express
+var app        = express();                 // define our app using express
+var bodyParser = require('body-parser');
+var Engine = require('./engine');
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;        // set our port
+
+engine = new Engine();
+engine.start();
+
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log('Magic happens on port ' + port);
